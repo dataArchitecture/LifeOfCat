054577286eac60111e858996cddb0eb583cc5a53
Sungbeom Ahn <sungbeom78@naver.com>
2018-03-18 20:46:28 +0900

    myPy 모두의파이썬 Day5

--- programming/python/201803_myPy/02C-turtle2.py
+++ programming/python/201803_myPy/02C-turtle2.py
@@ -2,26 +2,16 @@ import turtle as t
 
 # 삼각형 그리기
 t.color("red") # 펜 색상을 "빨간색"으로 바꿉니다.
-t.forward(100) # 거북이가 100만큼 앞으로 이동합니다.
-t.left(120)    # 거북이가 왼쪽으로 120도 회전합니다.
-t.forward(100) # 위 과정을 두번 반복합니다.
-t.left(120)
-t.forward(100)
-t.left(120)
-
+for x in range(3):
+    t.forward(100) # 거북이가 100만큼 앞으로 이동합니다.
+    t.left(120)    # 거북이가 왼쪽으로 120도 회전합니다.
 
 # 사각형 그리기
 t.color("green") # 펜 색상을 "녹색"으로 바꿉니다.
 t.pensize(3)     # 펜 굵기를 3으로 바꿉니다.
-t.forward(100) # 거북이가 100만큼 앞으로 이동합니다.
-t.left(90)     # 거북이가 왼쪽으로 90도 회전합니다.
-t.forward(100) # 위 과정을 세 번 반복합니다.
-t.left(90)
-t.forward(100)
-t.left(90)
-t.forward(100)
-t.left(90)
-
+for x in range(4):
+    t.forward(100) # 거북이가 100만큼 앞으로 이동합니다.
+    t.left(90)     # 거북이가 왼쪽으로 90도 회전합니다.
 
 # 원 그리기
 t.color("blue")  # 펜 색상을 "파란색"으로 바꿉니다.
---
+++ programming/python/201803_myPy/03B-tri.py
@@ -0,0 +1,11 @@
+import turtle as t
+
+d = 100
+
+# 삼각형 그리기
+t.forward(d) # 거북이가 d만큼 앞으로 이동합니다.
+t.left(120)  # 거북이가 왼쪽으로 120도 회전합니다.
+t.forward(d) # 위 과정을 두번 반복합니다.
+t.left(120)
+t.forward(d)
+t.left(120)
